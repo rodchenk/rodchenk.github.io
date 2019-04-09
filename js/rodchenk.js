b25ad163b8651155e7131f7b68e5b41fe55fe683
rodchenk <rodchenk@th-brandenburg.de>
2019-04-09 23:35:57 +0200

    add translation and new page

--- js/rodchenk.js
+++ js/rodchenk.js
@@ -1,15 +1,11 @@
 $(document).ready(function(e){
 	var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {constrainWidth: false});
-    $('.materialboxed').materialbox();
     $('.project-slider').slick({
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		dots: true
-	});
-	$('.grid').masonry({
-	  itemSelector: '.grid-item',
 	});
 });
 
@@ -17,15 +13,9 @@ function switchTheme(e){
 	var style = document.getElementById('main_style');
 	style.href = 'css/' + (e ? 'dark.rodchenk.css' : 'light.rodchenk.css');
 }
-function switchLanguage(lang){
-	setTranslation(lang);
-}
-function translate(word, language){
-	return translation[language][word] === undefined ? word : translation[language][word];
-}
+function switchLanguage(lang){setTranslation(lang)}
+function translate(word, language){return translation[language][word] === undefined ? word : translation[language][word]}
 function setTranslation(lang){
 	var words = document.getElementsByClassName('translate');
-	for (var i = 0; i < words.length; i++) {
-		words[i].innerHTML = translate(words[i].getAttribute('data-word'), lang);
-	}
+	for(var i=0;i< words.length;i++){words[i].innerHTML=translate(words[i].getAttribute('data-word'),lang)}
 }
